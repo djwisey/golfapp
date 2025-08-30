654287fc3059be1578ebd558ab9c82ae451fd768
djwisey <djwisey1@gmail.com>
2025-08-30 12:47:53 +0100

    Add files via upload

---
+++ golfy/golfyTests/golfyTests.swift
@@ -0,0 +1,17 @@
+//
+//  golfyTests.swift
+//  golfyTests
+//
+//  Created by Dylan Wiseman on 30/08/2025.
+//
+
+import Testing
+@testable import golfy
+
+struct golfyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
