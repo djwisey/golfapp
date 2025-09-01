deaf17d4ba8f38929a84b36dbff23600c0b36104
djwisey <djwisey1@gmail.com>
2025-09-01 18:29:53 +0100

    Add Swift Package manifest for tests

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+    name: "golfy",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(name: "golfy", targets: ["golfy"])
+    ],
+    targets: [
+        .target(
+            name: "golfy",
+            path: "golfy/golfy",
+            resources: [
+                .process("course.json"),
+                .process("Assets.xcassets"),
+                .process("golfy.xcdatamodeld")
+            ]
+        ),
+        .testTarget(
+            name: "golfyTests",
+            dependencies: ["golfy"],
+            path: "golfy/golfyTests"
+        ),
+        .testTarget(
+            name: "golfyUITests",
+            dependencies: ["golfy"],
+            path: "golfy/golfyUITests"
+        )
+    ]
+)
